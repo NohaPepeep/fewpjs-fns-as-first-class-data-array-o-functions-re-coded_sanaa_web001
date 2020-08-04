086b8ac3fdd8d00bd03f8618a1463fbd0a139d2f
Nuha Al-Sameai <noha.pepeep1@gmail.com>
2020-08-04 12:57:39 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,32 @@
+function wakeDog(dogName, dogBreed) {
+  let msg=`Wake ${dogName} the ${dogBreed}`;
+  
+}
+function leashDog(dogName, dogBreed) {
+ let msg=`Leash ${dogName} the ${dogBreed}`;
+  console.log(msg);
+  return msg;
+}
+function walkToPark(dogName, dogBreed) {
+ let msg=`Walk to the park with ${dogName} the ${dogBreed}`;
+  console.log(msg);
+  return msg;
+}
+function throwFrisbee(dogName, dogBreed) {
+  let msg=`Throw the frisbee for ${dogName} the ${dogBreed}`;
+  console.log(msg);
+  return msg;
+}
+function walkHome(dogName, dogBreed) {
+  let msg=`Walk home with ${dogName} the ${dogBreed}`;
+  console.log(msg);
+  return msg;
+}
+function unleashDog(dogName, dogBreed) {
+  let msg=`Unleash ${dogName} the ${dogBreed}`;
+  console.log(msg);
+  return msg;
+}
+var routine=[wakeDog,leashDog,
+ walkToPark,throwFrisbee,
+ walkHome,unleashDog];
